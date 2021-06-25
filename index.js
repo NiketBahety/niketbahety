1124545816dd4b50c5cf08dfb9268532e92ce3b2
NiketBahety <86421121+NiketBahety@users.noreply.github.com>
2021-06-25 20:09:41 +0530

    Add files via upload
    Reviews Section Added

--- index.js
+++ index.js
@@ -35,6 +35,37 @@ let previous = document.getElementById("previous");
 let slider_content = document.getElementById("slider-content");
 let review_template = document.getElementById("review-template");
 
+let counter = 1;
 
 next.addEventListener("click" , function(){
+    if(counter == 3)
+    {
+    counter = 1;
+    slider_content.style.left = "0%";
+    }
+    else
+    {
+    let leftpos = getComputedStyle(slider_content).left.substr(0 , getComputedStyle(slider_content).left.length - 2);
+    let sliderwidth = getComputedStyle(slider_content).width.substr(0 , getComputedStyle(slider_content).width.length - 2);
+    slider_content.style.left = leftpos - (sliderwidth/3) + "px";
+    counter++;
+    }
+});
+
+
+previous.addEventListener("click" , function(){
+    if(counter == 1)
+    {
+    counter = 3;
+    slider_content.style.left = "-200%";
+    }
+    else
+    {
+    let leftpos = getComputedStyle(slider_content).left.substr(0 , getComputedStyle(slider_content).left.length - 2);
+    console.log(leftpos);
+    let sliderwidth = getComputedStyle(slider_content).width.substr(0 , getComputedStyle(slider_content).width.length - 2);
+    console.log(sliderwidth);
+    slider_content.style.left = +leftpos + +(sliderwidth/3) + "px";
+    counter--;
+    }
 });
